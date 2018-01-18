1cf86a950b812363d3c80b948097008dd7f1634d
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-17 19:42:25 -0600

    Comminting Ravish's work in progress code

---
+++ assets/javascript/create.js
@@ -0,0 +1,46 @@
+$(function() {
+    function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        var autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */
+            (document.getElementById("eventLoc")));
+
+    };
+    initAutocomplete();
+
+    //save button clink event
+    $(document).on("click", "#btnCreateEvent", async function(event) {
+        event.preventDefault();
+        //validate the data
+        //if error throw error back
+        //update the database with details
+        var userSessionEntity = JSON.parse(sessionStorage.getItem("userSessionEntity"));
+
+        console.log("on click event");
+
+        var crammingClique = {
+            "id": null,
+            "host": userSessionEntity.email,//"ravish.rao@gmail.com",//,
+            "title": $("#eventTitle").val(),
+            "description": $("#eventDesc").val(),
+            "where": $("#eventLoc").val(),
+            "date": $("#eventDt").val(),
+            "time": $("#eventTm").val(),
+            "attendees": [{
+                "attendee": userSessionEntity.email//"ravish.rao@gmail.com"//
+            }]
+        };
+
+        try {
+            var insertStatus = await insertNewEventDetails(crammingClique);
+        } catch (e) {
+            console.log(e);
+            console.log("I am at 6");
+        }
+        //redirect the feed page
+        window.location.href = "feed.html";
+    });
+
+
+});
--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -13,41 +13,34 @@ firebase.initializeApp(firebaseConfig);
 // Create a variable to reference the database
 var database = firebase.database();
 
+// Google login initialization
 var googleAuth;
 var googleUser;
-
 gapi.load("auth2", function() {
     googleAuth = gapi.auth2.init({
         client_id: "245751156594-tp337vitvvl9ltm4jhpoirm249v20tsf.apps.googleusercontent.com",
         scope: "profile"
     });
-    console.log("api initialized");
-
-    attachSignin(document.getElementById("btn-login"));
+    googleLogin(document.getElementById("btnLogin"));
 });
 
 
-function attachSignin(element) {
+function googleLogin(element) {
     console.log(element);
     googleAuth.attachClickHandler(element, {},
         function(googleUser) {
-            console.log("success signin");
-            //$("#btn-login").text("Sign Out");
-            console.log(googleUser.getBasicProfile().getName());
-            console.log(googleUser.getBasicProfile().getImageUrl());
-
-            var userEntity = {};
-            userEntity.id = googleUser.getBasicProfile().getId();
-            userEntity.name = googleUser.getBasicProfile().getName();
-            userEntity.imageUrl = googleUser.getBasicProfile().getImageUrl();
-            userEntity.email = googleUser.getBasicProfile().getEmail();
-            userEntity.idToken = googleUser.getAuthResponse().id_token;
+            console.log("google login successful");
 
             //Store the entity object in sessionStorage where it will be accessible from all pages of the site.
-            sessionStorage.setItem("userEntity", JSON.stringify(userEntity));
+            var userSessionEntity = {};
+            userSessionEntity.id = googleUser.getBasicProfile().getId();
+            userSessionEntity.name = googleUser.getBasicProfile().getName();
+            userSessionEntity.imageUrl = googleUser.getBasicProfile().getImageUrl();
+            userSessionEntity.email = googleUser.getBasicProfile().getEmail();
+            userSessionEntity.idToken = googleUser.getAuthResponse().id_token;
+            sessionStorage.setItem("userSessionEntity", JSON.stringify(userSessionEntity));
 
             database.ref("/crammingUsers").orderByChild("email").equalTo(googleUser.getBasicProfile().getEmail()).once("value", function(snapshot) {
-
                 if (snapshot.val() !== null) {
                     console.log("record found: " + snapshot);
                     window.location.href = "feed.html";
@@ -59,11 +52,11 @@ function attachSignin(element) {
                         "name": googleUser.getBasicProfile().getName(),
                         "imageUrl": googleUser.getBasicProfile().getImageUrl(),
                         "email": googleUser.getBasicProfile().getEmail(),
-                        "phone": ""
+                        "phone": "",
+                        "receiveTextNotification": true
                     };
-
                     database.ref("/crammingUsers").push(crammingUser);
-                    window.location.href = "register.html";
+                    window.location.href = "profile.html";
                 }
 
             });
@@ -80,7 +73,7 @@ function signOut() {
 }
 
 function checkIfLoggedIn() {
-    if (sessionStorage.getItem("userEntity") == null) {
+    if (sessionStorage.getItem("userSessionEntity") == null) {
         return false;
     } else {
         return true;
