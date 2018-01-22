c630d9c1cc4530cc45fd8b5962b8572641107274
Ravish Rao <ravish_rao@yahoo.com>
2018-01-22 00:25:50 -0600

    Ravish commiting his work in progress work

--- assets/javascript/feed.js
+++ assets/javascript/feed.js
@@ -13,43 +13,136 @@ $(function() {
 
         var crammingCliques = await getAllCliques();
         //prepopulate all feed data pulled from the table
+
+        /*        
+
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">
+                            <div class="chip">
+                                <img src="">
+                            </div>
+                        </h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Date &amp; Time</p>
+                        <p>Location</p>
+                        <p>Description</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data="">3-way Button</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                */
         crammingCliques.forEach(function(clique) {
-            var divClique = $("<div>").addClass("card").attr("id", clique.id);
-            divClique.append($("<h6>").addClass("card-text").attr("id", "cliqueTitle").text(clique.title));
-            divClique.append($("<h6>").addClass("card-text").attr("id", "cliqueWhen").text(clique.date + " " + clique.time));
-            divClique.append($("<h6>").addClass("card-text").attr("id", "cliqueWhere").text(clique.where));
-            divClique.append($("<h6>").addClass("card-text").attr("id", "cliqueHost").text(clique.host));
-            divClique.append($("<p>").addClass("card-text").attr("id", "cliqueDescription").text(clique.description));
 
             var attending = false;
             var attendingCount = 1; //1 becuase the host will attend
-            var attendeesList = Object.keys(clique.attendees).map(function(key) {
-                return clique.attendees[key];
-            });
+            console.log(clique.attendees);
+            if (clique.attendees !== null && clique.attendees !== undefined) {
+                var attendeesList = Object.keys(clique.attendees).map(function(key) {
+                    return clique.attendees[key];
+                });
 
-            attendeesList.forEach(function(attendee) {
-                attendingCount++;
-                if (userSessionEntity.email === attendee.attendee) {
-                    attending = true;
-                }
-            });
 
-            divClique.append($("<h6>").addClass("card-text").attr("id", "cliqueAttendees").text( attendingCount +" people attending"));
-            
+                attendeesList.forEach(function(attendee) {
+                    attendingCount++;
+                    if (userSessionEntity.email === attendee.attendee) {
+                        attending = true;
+                    }
+                });
+            }
+            //divClique.append($("<h6>").addClass("card-text").attr("id", "cliqueAttendees").text(attendingCount + " people attending"));
+            var divClique = $("<div>").addClass("col-sm-3").attr("id", clique.id);
+
+            var divCliqueCard = $("<div>").addClass("card feedCard");
+
+            var titleElement = $("<h3>").addClass("card-title feedCardTitle").text(clique.title);
+            var cardBlock = $("<div>").addClass("card-block");
+            cardBlock.append(titleElement);
+            divCliqueCard.append(cardBlock);
+
+            var btnModal = $("<button>").addClass("btn btn-primary").text("Details");
+            btnModal.attr("type", "button");
+            btnModal.attr("data-toggle", "modal");
+            btnModal.attr("data-target", "#modal" + clique.id);
+            var cardFooter = $("<div>").addClass("card-footer feedCardFooter").text(attendingCount + " attending this event");
+            cardFooter.append(btnModal);
+            divCliqueCard.append(cardFooter);
+            divClique.append(divCliqueCard);
+
+            $("#divCliques").append(divClique)
+
+
+            var divModalHeader = $("<div>").addClass("modal-header");
+            var h5ModalUser = $("<h5>").addClass("modal-title").attr("id", "exampleModalLongTitle");
+            var imgHost = $("<div>").addClass("chip").append($("<img>").attr("src", ""));
+            h5ModalUser.append(imgHost);
+            divModalHeader.append(h5ModalUser);
+            var btnCliqueModalClose = $("<button>").addClass("close");
+            btnCliqueModalClose.attr("type", "button");
+            btnCliqueModalClose.attr("data-dismiss", "modal");
+            btnCliqueModalClose.attr("aria-label", "Close");
+            btnCliqueModalClose.append($("<span>").attr("aria-hidden", "true").text("X"));
+            divModalHeader.append(btnCliqueModalClose);
+
+
+            var divModalBody = $("<div>").addClass("modal-body");
+            divModalBody.append($("<p>").text("Date & Time: " + clique.date + " at " + clique.time));
+            divModalBody.append($("<p>").text("Location: " + clique.where));
+            divModalBody.append($("<p>").text("Description: " + clique.description));
+
 
             //for events for which user is the owner, have manage button
-            if (userSessionEntity.email  === clique.host) {
-                divClique.append($("<btn>").addClass("btn btn-primary").attr("id", "cliqueManage").text("Manage"));
+            var divModalFooter = $("<div>").addClass("modal-footer");
+            if (userSessionEntity.email === clique.host) {
+                //divClique.append($("<btn>").addClass("btn btn-primary").attr("id", "cliqueManage").text("Manage"));
+                var btnCliqueManage = $("<button>").addClass("btn btn-secondary").attr("id", "cliqueManage").text("Manage");
+                divModalFooter.append(btnCliqueManage);
+
             } else {
                 //for events for which user is not attending, have the register button
                 //for events for which user is already attending, have the derister button
                 if (attending === true) {
-                    divClique.append($("<btn>").addClass("btn btn-primary").attr("id", "cliqueDeRegister").text("De-register"));
+                    //divClique.append($("<btn>").addClass("btn btn-primary").attr("id", "cliqueDeRegister").text("De-register"));
+                    var btnCliqueManage = $("<button>").addClass("btn btn-secondary").attr("id", "cliqueDeRegister").text("De-register");
+                    divModalFooter.append(btnCliqueManage);
+
                 } else {
-                    divClique.append($("<btn>").addClass("btn btn-primary").attr("id", "cliqueRegister").text("Register"));
+                    //divClique.append($("<btn>").addClass("btn btn-primary").attr("id", "cliqueRegister").text("Register"));
+                    var btnCliqueManage = $("<button>").addClass("btn btn-secondary").attr("id", "cliqueRegister").text("Register");
+                    divModalFooter.append(btnCliqueManage);
+
                 }
             }
-            $("#divCliques").append(divClique)
+
+
+
+            var divModalContent = $("<div>").addClass("modal-content");
+            divModalContent.append(divModalHeader);
+            divModalContent.append(divModalBody);
+            divModalContent.append(divModalFooter);
+
+            var divModalDialog = $("<div>").addClass("modal-dialog modal-dialog-centered").attr("role", "document");
+            divModalDialog.append(divModalContent);
+
+            var divCliqueModal = $("<div>").addClass("modal fade").attr("id", "modal" + clique.id);
+            divCliqueModal.attr("tabindex", "-1");
+            divCliqueModal.attr("role", "dialog");
+            divCliqueModal.attr("aria-labelledby", "exampleModalCenterTitle");
+            divCliqueModal.attr("aria-hidden", "true");
+            divCliqueModal.append(divModalDialog);
+
+            $("#divCliques").append(divCliqueModal);
+
         });
     };
     loadAllCliques();
